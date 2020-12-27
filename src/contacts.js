d26d2d972062b5025d11423ae898f6ec92182a8b
Orin Fletcher <orin.fletcher@gmail.com>
2020-12-26 19:59:08 -0800

    imports contacts to index.js

--- src/contacts.js
+++ src/contacts.js
@@ -1,23 +1,25 @@
 const contacts = [
   {
-    name: "Beyonce",
+    name: "Jess",
     imgURL:
-      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
-    phone: "+123 456 789",
-    email: "b@beyonce.com"
+      "https://i0.heartyhosting.com/okmagazine.com/wp-content/uploads/2011/10/jessica-biel-celeb-bio-headshot.jpg?resize=330%2C390&ssl=1",
+    phone: "+123 456 7890",
+    email: "jess@alwayshavedenver.com",
   },
   {
     name: "Jack Bauer",
     imgURL:
       "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
-    phone: "+987 654 321",
-    email: "jack@nowhere.com"
+    phone: "+987 654 3213",
+    email: "jack@nowhere.com",
   },
   {
     name: "Chuck Norris",
     imgURL:
       "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
-    phone: "+918 372 574",
-    email: "gmail@chucknorris.com"
-  }
+    phone: "+918 372 5745",
+    email: "chucky@chucknorris.com",
+  },
 ];
+
+export default contacts;
